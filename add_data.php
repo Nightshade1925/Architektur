7763b7e2561164c43086836396845d1d84bc733f
Benjamin Harlacher <benjamin.harlacher@siemens.com>
2021-09-03 13:35:42 +0200

    update

--- add_data.php
+++ add_data.php
@@ -1,3 +1,12 @@
 <?php
-    $nachname = $_GET['nachname'];
+    $nachname = $_POST['nachname'];
+    $vorname = $_POST['vorname'];
+    $geburtsdatum = $_POST['geburtsdatum'];
+    $email = $_POST['email'];
+    $ahvNr = $_POST['ahvNr'];
+    $personalnr = $_POST['personalnr'];
+    $telefon = $_POST['telefon'];
+    $abteilung = $_POST['abteilung'];
+    $berufsbezeichnung = $_POST['berufsbezeichnung'];
+    $berufsbeschreibung = $_POST['berufsbeschreibung'];
 ?>
