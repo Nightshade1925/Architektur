c4e42768c966c0e4d28522c94dd0afe7f913a396
Benjamin Harlacher <benjamin.harlacher@siemens.com>
2021-09-08 15:53:51 +0200

    first odbc finished

--- add_data_odbc.php
+++ add_data_odbc.php
@@ -1,7 +1,9 @@
 <?php
  
     $database = "db_m151";
-    $con = odbc_connect('MySQL','root','password!');
+    $con = odbc_connect('MariaDb','root','password!'); /*die variante funktioniert connection aber kann ned date hinzuefüege
+    //$con = odbc_connect("DRIVER={SQL Server Native Client 11.0};Server=LAPTOP-TOQJ1EIO\SQLEXPRESS;Database=m151",
+    "root", "password!");*/
 
   
     if($con)
@@ -26,9 +28,13 @@
 
     
   
-    $sql = "insert into [tbl_person](vorname,nachname,geburtsdatum,ahv_nr,personal_nr,fk_firma,fk_abteilung,fk_telefon,fk_email,fk_beruf) VALUES 
-        ('$vorname', '$nachname', '$geburtsdatum','$ahvNr','$personalnr','1','1','1','1','1')";
+    $sql = "INSERT INTO tbl_person(vorname,nachname,geburtsdatum,ahv_nr,personal_nr,fk_abteilung,fk_telefon,fk_email,fk_beruf) VALUES 
+        ('$vorname', '$nachname', '$geburtsdatum','$ahvNr','$personalnr','1','1','1','1')";
     
-    odbc_exec($con, $sql);
+    $result = odbc_exec($con, $sql);
 
+    if($result == true)
+    {
+        echo "successfully added";
+    }
 ?>
